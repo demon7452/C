2454f3b84634cb7c43fe1994ade815f4f695e1d0
demon7452 <demonli7452@gmail.com>
2014-10-21 11:09:15 +0800

    escape character 转义字符

---
+++ BegainingC/ch1-CPrograme/EscapeCharacter.c
@@ -0,0 +1,14 @@
+/* Program 1.6 A simple C program-Impontant
+ * about the escape Character
+ * @author demon7452 
+ * @date 14-10-21
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\"Hello world !\"");
+	printf("\n Be Careful!!\a");/* the audio of warning*/
+	return 0;
+}
+

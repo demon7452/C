bceaa2b7523cbd72d9a3c08aa21dbfbfc40eecce
demon7452 <demonli7452@gmail.com>
2014-10-28 16:31:18 +0800

    for.sh  for loop

---
+++ linux_c/ch4-shellProgram/for.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#for.sh 
+#³öÏÖ´íÎó£ºsyntax error near unexpected token  ¸½½üÓĞÓï·¨´íÎóµÄÒâÍâ±ê¼Ç
+#³ö´íÔ­Òò£ºwindowsÀïĞ´µÄshell½Å±¾·Åµ½linuxÏµÍ³ÀïÔËĞĞ¾Í¿ÉÒÔ»áÌáÊ¾ÈçÏÂ´íÎó£¬ÒòÎªÁ½¸öÆ½Ì¨ÏÂÃæµÄ»»ĞĞ·û²»Ò»Ñùµ¼ÖÂµÄ
+#½â¾ö°ì·¨£ºÔÚnotepad++ÖĞ½«µµ°¸¸ñÊ½×ª»»ÎªUNIX¸ñÊ½£»ÔÚÓï¾äºóÃæ¼ÓÉÏ#£¬½«»»ĞĞ·û×¢ÊÍµô£»ÓÃvim -b sh ¿ÉÒÔ²é¿´»»ĞĞ·û
+#for loop without in
+for str                   #
+do				                #
+	echo $str               #
+done                      #
+#for loop 
+for char in a b c d e f       #å¼€å§‹forå¾ªç¯
+do														#å¾ªç¯ä½“
+	echo $char                #
+done													#ç»“æŸforå¾ªç¯

5e387840eeaaf032152f7f1dfe9326a224ba7563
demon7452 <demonli7452@gmail.com>
2014-11-19 15:28:06 +0800

    scanf input

---
+++ linux_c/ch6-CBasics/scanf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int i,j;
+	int m;
+	printf("please enter a char:\n");
+	scanf("%c",&m);
+	printf("the char is %c.\n",m);
+	printf("please enter a number:\n");
+	scanf("%d",&i);
+	printf("please enter a number:\n");
+	scanf("%d",&j);
+	printf("%d + %d = %d",i,j,i+j);
+}
